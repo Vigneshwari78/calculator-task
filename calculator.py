1b89f4f01c04895729f316dafd1c13a80fbb5f7f
Vigneshwari78 <vigneshwarivishwa803@gmail.com>
2025-08-04 20:27:45 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ operator=input("enter the operator(+ - * / ):")
 num1=float(input("enter the 1st value:"))
 num2=float(input("enter the 2nd value:"))
 if operator=="+":
-    result=num1+num
+    result=num1+num2
     print(result)
 elif operator=="-":
     result=num1-num2
